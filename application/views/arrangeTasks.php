2e20555cb2e229d6ceeec288403bbf3abaa93a6b
keziahisidoro <32053661+keziahisidoro@users.noreply.github.com>
2018-07-13 02:16:21 +0800

    Sub Act

--- application/views/arrangeTasks.php
+++ application/views/arrangeTasks.php
@@ -78,7 +78,7 @@
 									<tbody>
 
 										<tr>
-											<td class="btn" id="addRow"><a class="btn addButton" data-id="<?php echo $key; ?>" data-table="table_<?php echo $key;?>" data-num=<?php echo $value['TASKID']; ?> counter="1"><i class="glyphicon glyphicon-plus-sign"></i></a></td>
+											<td class="btn" id="addRow"><a class="btn addButton" data-id="<?php echo $key; ?>" data-table="table_<?php echo $key;?>" data-num=<?php echo $value['TASKID']; ?> counter="1" data-sum = "<?php echo count($groupedTasks); ?>"><i class="glyphicon glyphicon-plus-sign"></i></a></td>
 											<td><?php echo $value['TASKTITLE']; ?></td>
 											<td>
 												<?php
@@ -119,7 +119,8 @@
 												<input type="text" class="form-control" placeholder="Enter task title" name = "title[]" required>
 											</div></td>
 											<td width="40%">
-												<select class="form-control select2" multiple="multiple" name = "table_<?php echo $key; ?>_department0[]" data-placeholder="Select Departments">
+												<!-- <select class="form-control select2" multiple="multiple" name = "table_<?php echo $key; ?>_department0[]" data-placeholder="Select Departments"> -->
+												<select class="form-control select2" multiple="multiple" name = "department_<?php echo $key; ?>[]" data-placeholder="Select Departments">
 													<?php foreach ($departments as $row): ?>
 
 														<option>
@@ -188,14 +189,16 @@
 			 var currTable = $(this).attr('data-id');
 			 var t = $(this).attr('data-table');
 			 var mainAct = $(this).attr('data-num');
-			 var counter = parseInt($(this).attr('counter'));
+			 var counter = parseInt($(this).attr('data-sum'));
+			 var tot = $(this).attr('data-sum');
+
+			 console.log(tot);
 
 				 $('#table_' + currTable).append("<tr id='table_" +
 				 						currTable + "_Row_" + (i + 1) +
 										"'><td></td><td><div class ='form-group'> <input type='hidden' name='mainActivity_ID[]' value='" +
 										mainAct + "'> <input type='text' class='form-control' placeholder='Enter task title' name ='title[]' required></div></td>" +
-										"<td><select class='form-control select2' multiple='multiple' name = 'table_" + currTable +
-										"_department" + counter + "[]' data-placeholder='Select Departments'> " +
+										"<td><select class='form-control select2' multiple='multiple' name = 'department_" + tot + "[]' data-placeholder='Select Departments'> " +
 										"<?php foreach ($departments as $row) { echo '<option>' . $row['DEPARTMENTNAME'] . '</option>';  }?>" +
 										"</select></td> <td><div class='form-group'><div class='input-group date'><div class='input-group-addon'>" +
 										"<i class='fa fa-calendar'></i></div><input type='text' class='form-control pull-right taskStartDate' " +
@@ -209,7 +212,9 @@
 					$("#end_" + mainAct + "-" + counter).prop('disabled', true);
 
 				 var newCount = counter + 1;
-				 $(this).attr('counter', newCount);
+				 tot++;
+				 $("a.addButton").attr('counter', newCount);
+				 $("a.addButton").attr('data-sum', tot);
 
 				  $('.select2').select2();
 
