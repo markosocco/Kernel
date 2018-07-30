1e691e03ab9b8143dea493ca7a4bf557d66a3654
namihihii <nami.inomata@gmail.com>
2018-07-30 22:33:58 +0800

    notification redirect

--- application/controllers/controller.php
+++ application/controllers/controller.php
@@ -2870,6 +2870,99 @@ class controller extends CI_Controller
 		}
 	}
 
+	public function notifRedirect(){
+
+		$projectID = $this->input->post("projectID");
+		$taskID = $this->input->post("taskID");
+		$type = $this->input->post("type");
+		$notifID = $this->input->post("notifID");
+
+		if ($type == 2){ // taskDelegate
+
+			$filter = "users.departments_DEPARTMENTID = '". $_SESSION['departments_DEPARTMENTID'] ."'";
+
+			$data['delegateTasksByProject'] = $this->model->getAllProjectsToEditByUser($_SESSION['USERID'], "projects_PROJECTID");
+			$data['delegateTasksByMainActivity'] = $this->model->getAllActivitiesToEditByUser($_SESSION['USERID'], "1");
+			$data['delegateTasksBySubActivity'] = $this->model->getAllActivitiesToEditByUser($_SESSION['USERID'], "2");
+			$data['delegateTasks'] = $this->model->getAllActivitiesToEditByUser($_SESSION['USERID']);
+			$data['departments'] = $this->model->getAllDepartments();
+			$data['users'] = $this->model->getAllUsers();
+			$data['wholeDept'] = $this->model->getAllUsersByDepartment($_SESSION['departments_DEPARTMENTID']);
+			$data['projectCount'] = $this->model->getProjectCount($filter);
+			$data['taskCount'] = $this->model->getTaskCount($filter);
+
+			$this->load->view("taskDelegate", $data);
+
+		} else if ($type == 3){ //taskTodo
+
+			$data['tasks'] = $this->model->getAllTasksByUser($_SESSION['USERID']);
+
+			$this->load->view("taskTodo", $data);
+
+		} else if ($type == 4){ // taskMonitor
+
+			$data['allOngoingACItasks'] = $this->model->getAllACITasksByUser($_SESSION['USERID'], "Ongoing");
+			$data['uniqueOngoingACItasks'] = $this->model->getUniqueACITasksByUser($_SESSION['USERID'], "Ongoing");
+
+			$data['allCompletedACItasks'] = $this->model->getAllACITasksByUser($_SESSION['USERID'], "Complete");
+			$data['uniqueCompletedACItasks'] = $this->model->getUniqueACITasksByUser($_SESSION['USERID'], "Complete");
+
+			$this->load->view("taskMonitor", $data);
+
+		} else if ($type == 5){ // projectDocuments
+
+			$data['projectProfile'] = $this->model->getProjectByID($projectID);
+			$data['departments'] = $this->model->getAllDepartmentsByProject($projectID);
+			$data['documentsByProject'] = $this->model->getAllDocumentsByProject($projectID);
+			$data['documentAcknowledgement'] = $this->model->getDocumentsForAcknowledgement($projectID, $_SESSION['USERID']);
+			$data['users'] = $this->model->getAllUsersByProject($projectID);
+
+			$this->load->view("projectDocuments", $data);
+
+		} else if ($type == 6){ // rfc
+
+			$userID = $_SESSION['USERID'];
+			$deptID = $_SESSION['departments_DEPARTMENTID'];
+			switch($_SESSION['usertype_USERTYPEID'])
+			{
+				case '4': // if supervisor is logged in
+					$filter = "(usertype_USERTYPEID = '5' && users_SUPERVISORS = '$userID' && REQUESTSTATUS = 'Pending')
+						|| (projects.users_USERID = '$userID' && REQUESTSTATUS = 'Pending')"; break;
+				case '3': // if head is logged in
+					$filter = "(usertype_USERTYPEID = '4' && users.departments_DEPARTMENTID = '$deptID' && REQUESTSTATUS = 'Pending')
+					|| (projects.users_USERID = '$userID' && REQUESTSTATUS = 'Pending')"; break;
+				case '5': // if PO is logged in
+					$filter = "projects.users_USERID = '$userID' && REQUESTSTATUS = 'Pending'"; break;
+				default:
+					$filter = "usertype_USERTYPEID = '3' && REQUESTSTATUS = 'Pending'"; break;
+			}
+
+			$data['changeRequests'] = $this->model->getChangeRequestsForApproval($filter, $_SESSION['USERID']);
+			$data['userRequests'] = $this->model->getChangeRequestsByUser($_SESSION['USERID']);
+			$this->load->view("rfc", $data);
+
+		} else { // projectGantt
+
+			$data['projectProfile'] = $this->model->getProjectByID($projectID);
+			$data['ganttData'] = $this->model->getAllProjectTasksGroupByTaskID($projectID);
+			$data['dependencies'] = $this->model->getDependenciesByProject($projectID);
+			$data['users'] = $this->model->getAllUsers();
+
+			$data['responsible'] = $this->model->getAllResponsibleByProject($projectID);
+			$data['accountable'] = $this->model->getAllAccountableByProject($projectID);
+			$data['consulted'] = $this->model->getAllConsultedByProject($projectID);
+			$data['informed'] = $this->model->getAllInformedByProject($projectID);
+
+			$data['employeeCompleteness'] = $this->model->compute_completeness_employeeByProject($_SESSION['USERID'], $projectID);
+			$data['departmentCompleteness'] = $this->model->compute_completeness_departmentByProject($_SESSION['departments_DEPARTMENTID'], $projectID);
+			$data['employeeTimeliness'] = $this->model->compute_timeliness_employeeByProject($_SESSION['USERID'], $projectID);
+			$data['departmentTimeliness'] = $this->model->compute_timeliness_departmentByProject($_SESSION['departments_DEPARTMENTID'], $projectID);
+
+			$this->load->view("projectGantt", $data);
+
+		}
+	}
+
 	/******************** MY PROJECTS START ********************/
 
 	// ADDS MAIN ACTIVITIES TO PROJECT
