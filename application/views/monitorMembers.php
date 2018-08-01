9ff1c6642c449a79815b41510fcc09326bdd19bc
acagonzaga <gonzaga.aca@gmail.com>
2018-08-02 02:27:23 +0800

    back

--- application/views/monitorMembers.php
+++ application/views/monitorMembers.php
@@ -10,6 +10,8 @@
 			<div class="content-wrapper">
 				<!-- Content Header (Page header) -->
 				<section class="content-header">
+					<a href="<?php echo base_url("index.php/controller/monitorTeam"); ?>" class="btn btn-default btn" data-toggle="tooltip" data-placement="right" title="Return to My Team"><i class="fa fa-arrow-left"></i></a>
+					<br><br>
 					<h1>
 						Monitor Members
 						<small>What's happening to the members of my team?</small>
