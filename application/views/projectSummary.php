0a28d94b31d77e523d1b5d1b8a65f35957755154
namihihii <nami.inomata@gmail.com>
2018-08-21 05:53:45 +0800

    project summary (delayed tasks)

--- application/views/projectSummary.php
+++ application/views/projectSummary.php
@@ -244,7 +244,7 @@
 				</div>
 
 				<!-- DELAYED TASKS -->
-				<?php if($changeRequests != null):?>
+				<?php if($tasks != null):?>
 				<div class="row">
 					<div class="col-md-12 col-sm-12 col-xs-12">
 						<div class="box box-danger">
