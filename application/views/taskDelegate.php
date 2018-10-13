cf333bd41b5a026f42e421914990c21cda231072
acagonzaga <gonzaga.aca@gmail.com>
2018-10-13 18:15:22 +0800

    fixed acceptb modal on disabled button

--- application/views/taskDelegate.php
+++ application/views/taskDelegate.php
@@ -108,7 +108,7 @@
 																					<i class="fa fa-users"></i>
 																				</button>
 																				</span>
-																				<span data-toggle="modal" data-target="#modal-accept">
+																				<!-- <span data-toggle="modal" data-target="#modal-accept"> -->
 																				<button disabled id="taskAccept-<?php echo $task['TASKID'];?>" type="button" class="btn btn-success btn-sm acceptBtn"
 																				data-toggle="tooltip" data-placement="top" title="Accept"
 																				data-id="<?php echo $task['TASKID'];?>"
@@ -117,7 +117,7 @@
 																				data-end="<?php echo $task['TASKENDDATE'];?>">
 																					<i class="fa fa-thumbs-up"></i>
 																				</button>
-																				</span>
+																				<!-- </span> -->
 																			</td>
 																		</tr>
 																	<?php endif;?>
@@ -239,7 +239,7 @@
 																				<i class="fa fa-users"></i>
 																			</button>
 																			</span>
-																			<span data-toggle="modal" data-target="#modal-accept">
+																			<!-- <span data-toggle="modal" data-target="#modal-accept"> -->
 																			<button disabled id="taskAccept-<?php echo $task['TASKID'];?>" type="button" class="btn btn-success btn-sm acceptBtn"
 																			data-toggle="tooltip" data-placement="top" title="Accept"
 																			data-id="<?php echo $task['TASKID'];?>"
@@ -248,7 +248,7 @@
 																			data-end="<?php echo $task['TASKENDDATE'];?>">
 																				<i class="fa fa-thumbs-up"></i>
 																			</button>
-																			</span>
+																			<!-- </span> -->
 																		</td>
 																	</tr>
 																<?php endif;?>
@@ -936,6 +936,7 @@
 
 			// ACCEPT SCRIPT
 			$("body").on('click','.acceptBtn',function(){
+			 $("#modal-accept").modal('show');
 			 var $id = $(this).attr('data-id');
 			 var $title = $(this).attr('data-title');
 			 var $start = new Date($(this).attr('data-start'));
