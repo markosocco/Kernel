dd99be77bc0ea6a6d29b0c9d8d39beaa0724dc6d
namihihii <nami.inomata@gmail.com>
2018-10-27 01:43:46 +0800

    anyChart (new version)

--- application/views/projectGantt.php
+++ application/views/projectGantt.php
@@ -2111,11 +2111,12 @@
 			var progress = groupingTasks.progress();
     	progress.normal({fill: '#66b2b2'});
 
-			groupingTasks.normal({fill: 'Orange'});
+			// groupingTasks.normal({fill: 'Orange'});
+			// console.log(anychart.VERSION);
 
 			var elements = tl.elements();
 			var selectedElement = elements.selected();
-			selectedElement.fill('').stroke('Red');
+			selectedElement.fill('').stroke('');
 			chart.rowSelectedFill('#FFFFCC');
 
 			var tasks = tl.tasks();
@@ -2127,9 +2128,7 @@
 				var taskID = e.item.get('id');
 
 				// if parent (no modal)
-				if(e.item && e.item.numChildren()){
-					e.preventDefault();
-				} else {
+				if(e.item && !e.item.numChildren()){
 					$("#taskDetails").modal('show');
 
 					$(".divDetails").hide();
@@ -2528,12 +2527,7 @@
 						 alert("There was a problem in retrieving the task details");
 					 }
 					 });
-
 				}
-
-				// alert(e.item.getParent());
-				// console.log(e.item);
-				console.log(e.item.getParent());
     	});
 
 			// data grid getter
