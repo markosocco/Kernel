728f2787ec4c958df1574817d977d3f39d5dbadd
Marko Socco <juan_socco@dlsu.edu.ph>
2018-11-03 20:27:33 +0800

    Project Performance

--- application/views/reportsProjectPerformance.php
+++ application/views/reportsProjectPerformance.php
@@ -18,6 +18,8 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
   <!-- Own Style -->
   <!-- <link rel="stylesheet" href="<?php echo base_url("/assets/css/reportsProjectPerDeptStyle.css")?>"> -->
+  <!-- Frame Style -->
+  <link rel="stylesheet" href="<?php echo base_url("/assets/css/frameStyle.css")?>">
   <!-- Report Style -->
   <link rel="stylesheet" href="<?php echo base_url("/assets/css/reportStyle.css")?>">
 </head>
@@ -58,6 +60,61 @@
               </td>
             </tr>
           </table>
+
+          <!-- LINE CHART -->
+          <div class="box box-default">
+            <!-- <div class="box-header with-border">
+              <h3 class="box-title">Bar Chart</h3>
+            </div> -->
+            <div class="box-body">
+              <div class="chart">
+                <canvas id="lineChart" style="height:220px"></canvas>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+
+          <!-- BAR CHART -->
+          <div class="row">
+            <div class="col-md-8 col-sm-8 col-xs-8">
+              <div class="box box-default">
+                <div class="box-body">
+                  <div class="chart">
+                    <canvas id="barChart" style="height:210px"></canvas>
+                  </div>
+                </div>
+                <!-- /.box-body -->
+              </div>
+              <!-- /.box -->
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-12">
+							<div class="box box-default" style="height:230px; vertical-align:middle">
+								<!-- <div class="box-header with-border">
+									<h3 class="box-title">Project Performance</h3>
+								</div> -->
+								<!-- /.box-header -->
+								<div class="box-body" style="height:100%">
+	                <div style="display:inline-block; text-align:center; width:49%; height: 100%;">
+	                  <div>
+                      <br>
+                      <br>
+                      <h1>100%</h1>
+                      <h4>Timeliness</h4>
+	                  </div>
+	                </div>
+	                <div style="display:inline-block; text-align:center; width:49%; height: 100%; border-left: solid lightgray 1px;">
+	                  <div>
+                      <br>
+                      <br>
+                      <h1>100%</h1>
+                      <h4>Completeness</h4>
+	                 </div>
+	               </div>
+	              </div>
+							</div>
+		        </div>
+          </div>
 
   				<!-- DELAYED TASKS -->
           <?php if($delayedTasks != null):?>
@@ -200,5 +257,188 @@
   <!-- /.content -->
 </div>
 <!-- ./wrapper -->
+<!-- jQuery 3 -->
+<script src="<?php echo base_url()."assets/"; ?>bower_components/jquery/dist/jquery.min.js"></script>
+<!-- ChartJS -->
+<script src="<?php echo base_url()."assets/"; ?>bower_components/chart.js/Chart.js"></script>
+<!-- Progress Circle -->
+<script src="<?php echo base_url()."assets/"; ?>progress-circle/progresscircle.js" type="text/javascript"></script>
+<script>
+
+  $('.circlechart').circlechart(); // Initialization
+  $(function ()
+  {
+    var lineChartData = {
+
+      labels  : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+      datasets: [
+        {
+          label               : 'Digital Goods',
+          fillColor           : 'rgba(60,141,188,0.9)',
+          strokeColor         : 'rgba(60,141,188,0.8)',
+          pointColor          : '#3b8bba',
+          pointStrokeColor    : 'rgba(60,141,188,1)',
+          pointHighlightFill  : '#fff',
+          pointHighlightStroke: 'rgba(60,141,188,1)',
+          data                : [0, 15, 25, 27, 50, 70, 73, 80, 85, 90, 99, 100]
+        }
+      ]
+    }
+
+
+  var lineChartOptions = {
+    //Boolean - If we should show the scale at all
+    showScale               : true,
+    //Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines      : false,
+    //String - Colour of the grid lines
+    scaleGridLineColor      : 'rgba(0,0,0,.05)',
+    //Number - Width of the grid lines
+    scaleGridLineWidth      : 1,
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: true,
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines  : true,
+    //Boolean - Whether the line is curved between points
+    bezierCurve             : true,
+    //Number - Tension of the bezier curve between points
+    bezierCurveTension      : 0.3,
+    //Boolean - Whether to show a dot for each point
+    pointDot                : false,
+    //Number - Radius of each point dot in pixels
+    pointDotRadius          : 4,
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth     : 1,
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke           : true,
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth      : 2,
+    //Boolean - Whether to fill the dataset with a color
+    datasetFill             : true,
+    //String - A legend template
+    legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].lineColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+    //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+    maintainAspectRatio     : true,
+    //Boolean - whether to make the chart responsive to window resizing
+    responsive              : true,
+
+    onAnimationComplete: function ()
+    {
+      var ctx = this.chart.ctx;
+      ctx.font = this.scale.font;
+      ctx.fillStyle = this.scale.textColor
+      ctx.textAlign = "center";
+      ctx.textBaseline = "bottom";
+
+      this.datasets.forEach(function (dataset) {
+          dataset.bars.forEach(function (points) {
+              ctx.fillText(points.value, points.x, points.y - 0);
+          });
+      })
+    }
+  }
+
+
+  //-------------
+  //- LINE CHART -
+  //--------------
+  var lineChartCanvas          = $('#lineChart').get(0).getContext('2d')
+  var lineChart                = new Chart(lineChartCanvas)
+  lineChartOptions.datasetFill = false
+  lineChart.Line(lineChartData, lineChartOptions)
+
+    var barChartData = {
+      // LOOP THROUGH DEPTS
+      labels  : ['MKT', 'OPS', 'MIS', 'FIN', 'FAD', 'HR', 'PROC'],
+      datasets: [
+        {
+          label               : 'Timeliness',
+          fillColor           : 'rgba(210, 214, 222, 1)',
+          strokeColor         : 'rgba(210, 214, 222, 1)',
+          pointColor          : 'rgba(210, 214, 222, 1)',
+          pointStrokeColor    : '#c1c7d1',
+          pointHighlightFill  : '#fff',
+          pointHighlightStroke: 'rgba(220,220,220,1)',
+          data                : [90, 75, 100, 53, 56, 53, 20]
+                                // GRAY DATA
+        },
+        {
+          label               : 'Completeness',
+          fillColor           : 'rgba(60,141,188,0.9)',
+          strokeColor         : 'rgba(60,141,188,0.8)',
+          pointColor          : '#3b8bba',
+          pointStrokeColor    : 'rgba(60,141,188,1)',
+          pointHighlightFill  : '#fff',
+          pointHighlightStroke: 'rgba(60,141,188,1)',
+          data                : [28, 48, 40, 19, 86, 27, 90]
+                                // GREEN DATA
+        }
+      ]
+    }
+
+    //-------------
+    //- BAR CHART -
+    //-------------
+    var barChartCanvas                   = $('#barChart').get(0).getContext('2d')
+    var barChart                         = new Chart(barChartCanvas)
+    var barChartData                     = barChartData
+    barChartData.datasets[1].fillColor   = '#00a65a'
+    barChartData.datasets[1].strokeColor = '#00a65a'
+    barChartData.datasets[1].pointColor  = '#00a65a'
+    var barChartOptions                  = {
+      //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+      scaleBeginAtZero        : true,
+      //Boolean - Whether grid lines are shown across the chart
+      scaleShowGridLines      : true,
+      //String - Colour of the grid lines
+      scaleGridLineColor      : 'rgba(0,0,0,.05)',
+      //Number - Width of the grid lines
+      scaleGridLineWidth      : 1,
+      //Boolean - Whether to show horizontal lines (except X axis)
+      scaleShowHorizontalLines: true,
+      //Boolean - Whether to show vertical lines (except Y axis)
+      scaleShowVerticalLines  : true,
+      //Boolean - If there is a stroke on each bar
+      barShowStroke           : true,
+      //Number - Pixel width of the bar stroke
+      barStrokeWidth          : 2,
+      //Number - Spacing between each of the X value sets
+      barValueSpacing         : 5,
+      //Number - Spacing between data sets within X values
+      barDatasetSpacing       : 1,
+      //String - A legend template
+      legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+      //Boolean - whether to make the chart responsive
+      responsive              : true,
+      maintainAspectRatio     : true,
+
+      legend:
+      {
+        display: true,
+        position: 'top',
+      },
+
+      onAnimationComplete: function ()
+      {
+        var ctx = this.chart.ctx;
+        ctx.font = this.scale.font;
+        ctx.fillStyle = this.scale.textColor
+        ctx.textAlign = "center";
+        ctx.textBaseline = "bottom";
+
+        this.datasets.forEach(function (dataset) {
+            dataset.bars.forEach(function (bar) {
+                ctx.fillText(bar.value, bar.x, bar.y - 0);
+            });
+        })
+      }
+    }
+
+    barChartOptions.datasetFill = false
+    barChart.Bar(barChartData, barChartOptions)
+  })
+</script>
 </body>
 </html>
