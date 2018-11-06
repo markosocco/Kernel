b8005ac1a1e27eb613e7eefe4f425dbbd028e8b7
acagonzaga <gonzaga.aca@gmail.com>
2018-11-07 05:41:34 +0800

    fixed restricted access ui

--- application/views/restrictedAccess.php
+++ application/views/restrictedAccess.php
@@ -4,17 +4,27 @@
 		<link rel = "stylesheet" href = "<?php echo base_url("/assets/css/restrictedAccessStyle.css")?>">
 		<link rel="stylesheet" href="<?php echo base_url()."assets/"; ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
 		<link rel="stylesheet" href="<?php echo base_url()."assets/"; ?>animate.css/animate.min.css"/>
+		<!-- Google Font -->
+		<link rel="stylesheet"
+					href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 	</head>
 	<body>
 		<div id = mainContainer>
 			<img class="animated zoomIn" id = "logo"  src = "<?php echo base_url("/assets/media/tei.png")?>">
 			<h3 id="formHeader">KERNEL:<br>PROJECT MANAGEMENT</h3>
+			<br><br>
 			<div class = "">
-				<h2>Heeeey, you seemed lost. Please log in so I can recognize you. Have a nice day!</h2>
+				<h1>Hey, you seemed lost.</h1>
+				<?php if(!isset($_SESSION['USERID'])):?>
+					<h2>Please log in so I can recognize you. Have a nice day!</h2>
+					<br>
+					<a class="btn btn-primary redirectBtn" href = "<?php echo base_url('index.php/controller/login');?>" style="text-decoration-color:white">Log In</a>
+				<?php else:?>
+					<h2>Please return to the dashboard. Have a nice day!</h2>
+					<br>
+					<a class="btn btn-primary redirectBtn" href = "<?php echo base_url('index.php/controller/dashboard');?>" >Dashboard</a>
+				<?php endif;?>
 			</div>
-
-      <a class="btn btn-primary" href = "<?php echo base_url('index.php/controller/login');?>" >Log In</a>
-      <a class="btn btn-primary" href = "<?php echo base_url('index.php/controller/dashboard');?>" >Dashboard</a>
 		</div>
 
 		<footer>
