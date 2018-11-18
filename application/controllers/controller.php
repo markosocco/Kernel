89914131ca3c8aed71dfd5e9c7b5527dad85aa23
keziahisidoro <32053661+keziahisidoro@users.noreply.github.com>
2018-11-18 12:47:16 +0800

    prog bar fix

--- application/controllers/controller.php
+++ application/controllers/controller.php
@@ -2568,7 +2568,8 @@ class controller extends CI_Controller
 
 					// Main Completeness
 					$mainCompletenessFound = $this->model->checkMainCompleteness($projectID);
-					if($mainCompletenessFound == NULL){
+					if($mainCompletenessFound == NULL)
+					{
 
 						$data['mainActivities'] = $this->model->getMainActivitiesByProject($projectID);
 						$data['subActivities'] = $this->model->getSubActivitiesByProject($projectID);
@@ -2630,6 +2631,52 @@ class controller extends CI_Controller
 
 					$data['pastProgress'] = $this->model->getAssessmentByMain($data['interval'], $projectID);
 					$data['currentProgress'] = $this->model->getCurrentAssessmentByMain($projectID);
+
+					$data['taskWeight'] = $this->model->getTaskWeightByProject($projectID);
+					$data['taskCountMain'] = array();
+					$data['allTasks'] = $this->model->getAllProjectTasksGroupByTaskID($projectID);
+
+					$mainActCompleteness = array();
+					$subActCompleteness = array();
+					$totalWeight = 0;
+
+					// GET TASK COUNT PER MAIN
+					foreach ($data['mainActivities'] as $mainCompKey => $mainCompValue)
+					{
+						$mainCompleteness = 0;
+
+						foreach ($data['subActivities'] as $subCompKey => $subCompValue)
+						{
+							if ($subCompValue['tasks_TASKPARENT'] == $mainCompValue['TASKID'])
+							{
+								$subCompleteness = 0;
+
+								foreach ($data['allTasks'] as $taskCompKey => $taskCompValue)
+								{
+									if ($taskCompValue['CATEGORY'] == 3  && $taskCompValue['TASKSTATUS'] == 'Complete')
+									{
+										if ($taskCompValue['tasks_TASKPARENT'] == $subCompValue['TASKID'])
+										{
+											$subCompleteness += $data['taskWeight'];
+										}
+									}
+								}
+
+								$mainCompleteness += $subCompleteness;
+
+								array_push($subActCompleteness, $subCompleteness);
+							}
+						}
+
+						$mainData = array(
+							'TASKID' =>  $mainCompValue['TASKID'],
+							'weight' => $mainCompleteness
+						);
+
+						array_push($mainActCompleteness, $mainData);
+					}
+
+					$data['mainWeight'] = $mainActCompleteness;
 
 					$this->load->view("reportsProjectProgress", $data);
 				}
